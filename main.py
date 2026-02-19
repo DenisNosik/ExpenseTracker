65dd2460912eeb17acabde37ee0944781101bcd8
DenisNosik <denisnosik96@gmail.com>
2026-02-19 18:10:22 +0000

    start

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+import argparse
 
 
+def main():
 
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
